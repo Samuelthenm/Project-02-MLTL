2a75056bc0de69b58d1350eb4188cab93ce304b9
Samuelthenm <samuelthenm@gmail.com>
2025-01-27 20:10:07 -0500

    commit

---
+++ models/burger.js
@@ -0,0 +1,14 @@
+// create a Burger model using Sequelize
+
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        burger_name: {
+          type: DataTypes.STRING,
+        },
+        devoured: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false,
+        }
+    });
+    return Burger;
+  };
